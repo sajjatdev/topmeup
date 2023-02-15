37493d962b5794e9bd9557ef96611ddcebff73d0
sajjatflutterdeveloper <usecse1@gmail.com>
2023-02-14 21:53:51 -0800

    init

--- ussd/views.py
+++ ussd/views.py
@@ -5,14 +5,27 @@ from rest_framework import status
 
 
 class Index(APIView):
-    def post(self, request, format=None):
-        call_log = request.POST.get("callog")
-        sponsor_number = request.POST.get("sponsor_number")
-        user_number = request.POST.get("user_number")
-        network = request.POST.get("network")
-        amount = request.POST.get("amount")
 
-        return Response({"Sponsor Number": sponsor_number, "amount": amount, "url": "https://paymentLink.com/pay/", }, status.HTTP_200_OK)
+    def post(self, request, format=None):
+
+        call_log = request.data.get("call_log")
+        sponsor_number = request.data.get("sponsor_number")
+        user_number = request.data.get("user_number")
+        network = request.data.get("network")
+        amount = request.data.get("amount")
+
+        if call_log is None:
+            return Response({"Error Message": "Call Log NO Required"})
+        elif sponsor_number is None:
+            return Response({"Error Message": "Sponsor Number Required"})
+        elif user_number is None:
+            return Response({"Error Message": "User Number Required"})
+        elif network is None:
+            return Response({"Error Message": "Network Required"})
+        elif amount is None:
+            return Response({"Error Message": "Amount Required"})
+        else:
+            return Response({"Sponsor Number": sponsor_number, "amount": amount, "url": "https://paymentLink.com/pay/", }, status.HTTP_200_OK)
 
     def get(self, request, format=None):
         return Response({"status": "get request Success"})
