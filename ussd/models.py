aac971e1f61448d0d7f7994487b4c7805e7a927f
sajjatflutterdeveloper <usecse1@gmail.com>
2023-02-14 20:45:33 -0800

    Add USSD app with create data  model

--- ussd/models.py
+++ ussd/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class USSD(models.Model):
+    call_log = models.CharField(max_length=255)
+    sponsor_number = models.CharField(max_length=255)
+    user_number = models.CharField(max_length=255)
+    network = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+
+    class Meta:
+
+        verbose_name = 'USSD'
+        verbose_name_plural = 'USSDs'
+
+    def __str__(self):
+        return self.call_log
